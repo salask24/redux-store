3adae97d8ccaed038cc65c6b72adbc0e0e83a53c
Kimberly Salas <salask0104@gmail.com>
2021-08-29 12:42:31 -0700

    added stripe and checkout success page

--- client/src/App.js
+++ client/src/App.js
@@ -8,9 +8,9 @@ import {
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
+// import { StoreProvider } from "./utils/GlobalState";
+
 import { StoreProvider } from "./utils/GlobalState";
-
-
 import Home from './pages/Home';
 import Detail from './pages/Detail';
 import NoMatch from './pages/NoMatch';
@@ -18,6 +18,7 @@ import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Nav from './components/Nav';
 import OrderHistory from './pages/OrderHistory';
+import Success from "./pages/Success";
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -49,6 +50,7 @@ function App() {
               <Route exact path="/" component={Home} />
               <Route exact path="/login" component={Login} />
               <Route exact path="/signup" component={Signup} />
+              <Route exact path="/success" component={Success} />
               <Route exact path="/orderHistory" component={OrderHistory} />
               <Route exact path="/products/:id" component={Detail} />
               <Route component={NoMatch} />
